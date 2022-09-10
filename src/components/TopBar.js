a4763005d19936a9726313e34a7a545f814884be
Little Human <little.human@novonine.space>
2022-09-10 22:51:22 +0700

    Use svg logo
    Signed-off-by: Little Human <little.human@novonine.space>

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -3,7 +3,7 @@ export default function TopBar() {
   return (
     <div className="top-bar-grid z-20">
       <div className="top-bar-logo">
-        <img className="top-bar-logo-image" src="./logo.png" />
+        <img className="top-bar-logo-image" src="./logo.svg" />
       </div>
       <div className="top-bar-button">
         {/* <Button text="Submit Your Questions" /> */}
